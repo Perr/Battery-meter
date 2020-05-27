cbdc993ad4c3057eaba39ffcfe947d2f5fd96fda
Perr <perrypleister@hotmail.com>
2020-05-27 22:18:44 +0200

    Update Battery-meter.ino
    Added milliamphours measurement

--- Battery-meter.ino
+++ Battery-meter.ino
@@ -19,7 +19,7 @@ void setup(void)
 {
   pinMode(PWMPin, OUTPUT);
   Serial.begin(115200);
-  Serial.println("Hello!");
+  //Serial.println("Hello!");
   
   //Serial.println("Getting differential reading from AIN0 (P) and AIN1 (N)");
   //Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
@@ -52,6 +52,7 @@ void setup(void)
 
 int outputSignal = 2230;
 double milliwatthours = 0.0;
+double milliamphours = 0.0;
 
 double volts = 0.0;
 double amps = 0.0;
@@ -59,7 +60,7 @@ double esr = 0.0;
 
 double ReadVoltage()
 {
-  double multiplier = 0.1875;
+  double multiplier = 0.1875;//0.1875mV resolution
   ads.setGain(GAIN_TWOTHIRDS);
   int16_t results = ads.readADC_Differential_0_1();
   double volts = results * multiplier;
@@ -72,7 +73,7 @@ double ReadVoltage()
 
 double ReadCurrent()
 {
-  double currentMultiplier = 0.078125;
+  double currentMultiplier = 0.078125;//0.078125mA resolution
   ads.setGain(GAIN_SIXTEEN);
   int16_t currentResults = ads.readADC_Differential_2_3();
   if(abs(currentResults) < 4)
@@ -92,7 +93,7 @@ void measureESR()
   double loadVolts = volts;
   double loadAmps = amps;
   analogWrite(PWMPin, 0);
-  delay(800);
+  delay(4800);
   double voltsDifference = ReadVoltage() - loadVolts;
   esr = (voltsDifference * 1000.0d) / loadAmps;
   analogWrite(PWMPin, outputSignal);
@@ -103,6 +104,7 @@ unsigned long dischargeTime = 0;
 void secondTick(void *)
 {
   milliwatthours += volts * amps * 0.000000277777777777777777777777777d;
+  milliamphours += amps * 0.000277777777777777777777777777d;
   counter++;
   dischargeTime++;
   if(counter == 20)
@@ -121,16 +123,17 @@ void loop(void)
 
   StaticJsonDocument<64> doc;
   doc["esr"] = esr;
-  doc["h"] = milliwatthours;
+  doc["mah"] = milliamphours;
+  doc["mwh"] = milliwatthours;
   doc["U"] = volts;
   doc["I"] = amps;
-  doc["T"] = dischargeTime;
+  doc["t"] = dischargeTime;
   serializeJson(doc, Serial);
   Serial.println("");
   
-  if(amps < 960.0){
+  if(amps < 995.0){
     outputSignal++;
-  }else if(amps > 1040.0){
+  }else if(amps > 1005.0){
     outputSignal--;
   }
   if(volts < 3000)
